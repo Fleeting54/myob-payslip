657d29a099f1508d2a893a724b6e1a7f1a3ebc29
Andrew Buissink <andrewbuissink@live.com.au>
2021-06-11 18:04:28 +1000

    added ability to specify NZ payslip

--- generatePayslip.js
+++ generatePayslip.js
@@ -1,4 +1,4 @@
-const TAX_RATES = [
+const AUS_TAX_RATES = [
     {min:1, max:20000, rate:0},
     {min:20001, max:40000, rate:.1},
     {min:40001, max:80000, rate:.2},
@@ -6,17 +6,32 @@ const TAX_RATES = [
     {min:180001, max:false, rate:.4}
 ]
 
-function validateArgs(args){
+const NZ_TAX_RATES = [
+    {min:1, max:14000, rate:.1},
+    {min:14001, max:48000, rate:.17},
+    {min:48001, max:70000, rate:.3},
+    {min:70001, max:false, rate:.33}
+]
 
-    if (args.length!==4){
+let countryTaxRates = new Map();
+countryTaxRates.set("Aus", AUS_TAX_RATES)
+countryTaxRates.set("NZ", NZ_TAX_RATES)
+
+function validateArgs(args){
+    if (args.length!==5){
         //incorrect number of args
         console.log(`Expecting 2 args but received ${args.length-2}`)
         return false
-    } else if (typeof(args[2])!=="string"){
+    } else if ((typeof(args[2])!=="string")){
         // incorrect arg type - name isn't string
         console.log("incorrect arg type - name")
         return false
-    } else if (isNaN(Number(args[3]))){
+    } else if ((typeof(args[5])!=="string")&&(args[5]==="Aus" || args[5]==="NZ")){
+        // incorrect arg type - name isn't string
+        console.log("incorrect arg type - country: expected \"Aus\" or \"NZ\"")
+        return false
+    }
+    else if (isNaN(Number(args[3]))){
         // incorrect arg type - gross isn't number
         console.log("incorrect arg type - gross")
         return false
@@ -27,38 +42,57 @@ function validateArgs(args){
 function usage(){
 
     console.log("USAGE")
-    console.log("node generatePayslip.js [Name] [Gross]")
+    console.log("node generatePayslip.js [Name] [Gross] [Country]")
     console.log("-Name :  the name to be appended to the payslip. Should be in string format")
     console.log("-Gross :  the gross annual amount to calculate income on. Should be a number")
-
+    console.log("-Country :  the country whose tax rates will apply to gross annual payment. Should be either NZ or Aus")
 }
 
-function generatePayslip(name, grossAnnual){
+function generatePayslip(name, grossAnnual, country){
     let grossMonthly = grossAnnual / 12;
-    let net = 0;
-    let tax = 0
+    let tax = 0;
+    let taxMonthly;
+    let netMonthly;
 
-    TAX_RATES.forEach((bracket)=>{
-        if (bracket.max && grossAnnual - bracket.max > 0){ //bracket.max condition allows us to set boolean flag rather than numerical limit on last bracket
-            tax += (((bracket.max-bracket.min)+1)*bracket.rate); // +1 compensates for tax payed on the 1st dollar per bracket
-        } else if (grossAnnual > bracket.min){
-            tax += (((grossAnnual-bracket.min)+1)*bracket.rate); // +1 compensates for tax payed on the 1st dollar per bracket
-        }
+    countryTaxRates.get(country).forEach((bracket)=>{
+        tax += calculateTaxBracket(bracket, grossAnnual)
     })
+    taxMonthly = tax/12
 
-    tax = tax/12
+    netMonthly = grossMonthly - taxMonthly
 
-    net = (grossMonthly) - tax
-
-    console.log(`Monthly Payslip for: \"${name}\"`)
-    console.log(`Gross Monthly Income: $${Number(grossMonthly).toFixed(2)}`)
-    console.log(`Monthly Income Tax: $${Number(tax).toFixed(2)}`)
-    console.log(`Net Monthly Income: $${Number(net).toFixed(2)}`)
-
+    let results = {
+        name: name,
+        grossMonthly: grossMonthly,
+        taxMonthly: taxMonthly,
+        netMonthly: netMonthly
+    }
+    printPayslip(results)
 }
 
-if (validateArgs(process.argv)){
-    generatePayslip(process.argv[2], process.argv[3])
-} else{
-    usage();
+function calculateTaxBracket(bracket, grossAnnual){
+    let tax=0;
+    if (bracket.max && grossAnnual - bracket.max > 0){ //bracket.max condition allows us to set boolean flag rather than numerical limit on last bracket
+        tax = (((bracket.max-bracket.min)+1)*bracket.rate); // +1 compensates for tax payed on the 1st dollar per bracket
+    } else if (grossAnnual > bracket.min){
+        tax = (((grossAnnual-bracket.min)+1)*bracket.rate); // +1 compensates for tax payed on the 1st dollar per bracket
+    }
+        return tax
 }
+
+function printPayslip(payValues){
+    console.log(`Monthly Payslip for: \"${payValues.name}\"`)
+    console.log(`Gross Monthly Income: $${Number(payValues.grossMonthly).toFixed(2)}`)
+    console.log(`Monthly Income Tax: $${Number(payValues.taxMonthly).toFixed(2)}`)
+    console.log(`Net Monthly Income: $${Number(payValues.netMonthly).toFixed(2)}`)
+}
+
+   if (validateArgs(process.argv)) {
+        generatePayslip(process.argv[2], process.argv[3], process.argv[4])
+        generatePayslip(name,grossAnnual,country)
+    } else {
+       usage();
+   }
+
+
+
